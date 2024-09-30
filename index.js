f43968d63ac6e37f5e9fceede6b38c1a5aee9c10
Simone De Vittorio <simonedevittorio.me@gmail.com>
2024-09-30 16:48:31 +0200

    add charts

---
+++ index.js
@@ -0,0 +1,58 @@
+const WINS = 1;
+const DRAWS = 0;
+const LOSES = 1;
+const TOTAL = WINS + DRAWS + LOSES;
+
+
+function getPercentage(value, total) {
+    const percentage = (value / total) * 100;
+    return percentage.toFixed(2) + '%';
+}
+
+function setInfo(isSimone) {
+    const wins = isSimone ? WINS : LOSES;
+    const loses = isSimone ? LOSES : WINS;
+    document.getElementById("wins").innerHTML = `${wins} (${getPercentage(wins, TOTAL)}).`;
+    document.getElementById("draws").innerHTML = `${DRAWS} (${getPercentage(DRAWS, TOTAL)}).`;
+    document.getElementById("loses").innerHTML = `${loses} (${getPercentage(loses, TOTAL)}).`;
+};
+
+function createRadarChart() {
+    var options = {
+        fill: {
+            opacity: 0.5,
+        },
+        markers: {
+            size: 0
+        },
+        chart: {
+            type: 'radar'
+        },
+        series: [{
+            name: 'sales',
+            data: [63, 45, 79, 74, 50, 87]
+        }],
+        yaxis: {
+            show: false
+        },
+        xaxis: {
+            categories: ['SERVIZIO', 'SMASH', 'ROVESCIO', 'SMORZATA', 'VOLEE', 'DIRETTO'],
+            labels: {
+                show: true,
+                style: {
+                    colors: ["#a8a8a8"],
+                    fontSize: "30px",
+                    fontFamily: 'Arial'
+                }
+            }
+        }
+    }
+
+    var chart = new ApexCharts(document.getElementById("chart"), options);
+    chart.render();
+}
+
+function init(isSimone) {
+    setInfo(isSimone);
+    createRadarChart();
+}
